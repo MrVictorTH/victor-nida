c146f2926829cc176b1cb548957fd1bdf8ee6c7e
MrVictorTH <aukawin.c@gmail.com>
2025-04-16 01:07:35 +0700

    Delete Comment Code

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,15 +1,3 @@
-// import {useTranslations} from 'next-intl';
-// import {Link} from '@/i18n/navigation';
- 
-// export default function HomePage() {
-//   const t = useTranslations('HomePage');
-//   return (
-//     <div>
-//       <h1>{t('title')}</h1>
-//       <Link href="/about">{t('about')}</Link>
-//     </div>
-//   );
-// }
 import dynamic from 'next/dynamic';
 import { fetchHome } from '@/service/homepage'
 import React from 'react';
@@ -18,8 +6,8 @@ const NewsComponent = dynamic(() => import('@/components/homepage/news'));
 
 const HomePage = async () => {
     const homeDataList = await fetchHome();
-    console.log(homeDataList);
-    console.log(homeDataList.news);
+    // console.log(homeDataList);
+    // console.log(homeDataList.news);
     return <NewsComponent newsList={homeDataList.news} />
 }
 
