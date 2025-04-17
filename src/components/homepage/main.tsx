4dc6d6972b7d2b16df60e5e49434a96c168181ed
MrVictorTH <aukawin.c@gmail.com>
2025-04-18 00:29:52 +0700

    Added back-ground picture.

--- src/components/homepage/main.tsx
+++ src/components/homepage/main.tsx
@@ -7,7 +7,10 @@ export default function Main() {
   const t = useTranslations('Homepage.Main');
   return (
     <>
-      <div className="flex h-[80svh] w-full flex-row items-center justify-center">
+      <div className="flex h-[80svh] flex-row items-center justify-center">
+        <div className="absolute inset-0 -z-10">
+          <img src="/homepage/web_bg.png" alt="background" className="h-[80svh] w-full object-cover" />
+        </div>
         <Box className="flex w-full flex-col gap-4">
           <Typography variant="h3" fontWeight={'bold'} color="text.primary">
             {t('title')}
@@ -15,25 +18,13 @@ export default function Main() {
           <Typography variant="subtitle2" fontWeight={'medium'} color="text.primary">
             {t('description')}
           </Typography>
-          <Button
-            variant="contained"
-            color="primary"
-            endIcon={<ArrowForward />}
-            sx={{ width: '130px' }}
-            href="/explore"
-          >
+          <Button variant="contained" color="primary" endIcon={<ArrowForward />} sx={{ width: '130px' }} href="/explore">
             <Typography variant="body1" fontWeight={'bold'}>
               {t('button')}
             </Typography>
           </Button>
         </Box>
       </div>
-      <div
-        className="absolute right-0 top-0 z-0 h-[80svh] w-1/2 bg-primary-main"
-        style={{
-          clipPath: 'polygon(0 100%, 25% 0, 100% 0, 100% 100%)',
-        }}
-      />
     </>
   );
 };
