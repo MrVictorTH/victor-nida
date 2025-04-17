03aef37c6376b6d4614239512b8e04ebe3751742
MrVictorTH <aukawin.c@gmail.com>
2025-04-17 22:24:18 +0700

    Added component main.

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,6 +2,7 @@ import dynamic from 'next/dynamic';
 import { fetchHome } from '@/service/homepage'
 import React from 'react';
 
+const MainComponent = dynamic(() => import('@/components/homepage/main'));
 const NewsComponent = dynamic(() => import('@/components/homepage/news'));
 const FreeRegistrationComponent = dynamic(() => import('@/components/homepage/registration'));
 
@@ -11,6 +12,7 @@ const HomePage = async () => {
     // console.log(homeDataList.news);
     return (
         <>
+            <MainComponent />
             <div className="flex flex-col items-center justify-center gap-8 py-12">
                 <NewsComponent newsList={homeDataList.news} />
                 <FreeRegistrationComponent />
---
+++ src/components/homepage/main.tsx
@@ -0,0 +1,39 @@
+import { ArrowForward } from '@mui/icons-material';
+import { Box, Button, Typography } from '@mui/material';
+import { useTranslations } from 'next-intl';
+import React from 'react';
+
+export default function Main() {
+  const t = useTranslations('Homepage.Main');
+  return (
+    <>
+      <div className="flex h-[80svh] w-full flex-row items-center justify-center">
+        <Box className="flex w-full flex-col gap-4">
+          <Typography variant="h3" fontWeight={'bold'} color="text.primary">
+            {t('title')}
+          </Typography>
+          <Typography variant="subtitle2" fontWeight={'medium'} color="text.primary">
+            {t('description')}
+          </Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            endIcon={<ArrowForward />}
+            sx={{ width: '130px' }}
+            href="/explore"
+          >
+            <Typography variant="body1" fontWeight={'bold'}>
+              {t('button')}
+            </Typography>
+          </Button>
+        </Box>
+      </div>
+      <div
+        className="absolute right-0 top-0 z-0 h-[80svh] w-1/2 bg-primary-main"
+        style={{
+          clipPath: 'polygon(0 100%, 25% 0, 100% 0, 100% 100%)',
+        }}
+      />
+    </>
+  );
+};
